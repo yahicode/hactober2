b5aed3335df931f84bf5004f22d7b121c5502158
abhijeet223 <49865971+abhijeet223@users.noreply.github.com>
2019-10-25 11:39:08 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 #pyhton3.x
 
 print('hello Everyone and thnaks Mayank for making this youtube video...i am novice i am trying to learn some thing here..')
-print("Connect me on email: viveky1794@gmail.com")
+print("Connect me on email: viveky1794@gmail.com") 
+print("python is really a great language")
