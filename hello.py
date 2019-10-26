a2607373bbeb816b652da17110706b286bc7240d
msromi <57037790+msromi@users.noreply.github.com>
2019-10-26 18:50:06 +0530

    updated hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@
 print('hello Everyone and thnaks Mayank for making this youtube video...i am novice i am trying to learn some thing here..')
 print("Connect me on email: viveky1794@gmail.com")
 print("its open source and its hacktoberfest")
+print("New to open source")
+print("Thank you hacktober fest and github")
