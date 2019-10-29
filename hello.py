6a3428934dbce4d077326049bece397f5c1d3b58
Sarswat Aryan <50901866+sarswat001@users.noreply.github.com>
2019-10-29 18:14:30 +0530

    hell.py

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@
 
 print('hello Everyone and thnaks Mayank for making this youtube video...i am novice i am trying to learn some thing here..')
 print("Connect me on email: viveky1794@gmail.com")
-print("its open source and its hacktoberfest")
+
